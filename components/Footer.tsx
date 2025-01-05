08b0240f439f8e9c98e1fb6cab8c9e7a4f49c1aa
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-01-05 17:10:22 +0530

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to transform <span className="text-purple">your</span> lending
-          strategy with Nexa Wealth Advisor's intelligent credit risk platform?
+          strategy with Nexa Wealth Advisor s intelligent credit risk platform?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to us today and let&apos;s discuss how I can help you
